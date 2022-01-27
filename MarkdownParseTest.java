be02f5b81b586b9e55d9df495a5bf894cac7f2c4
alextahan <atahan@ucsd.edu>
2022-01-26 16:11:45 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,23 @@
 import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 import org.junit.*;
 
+//javac -cp "lib/\*" MarkdownParseTest.java
+//java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testGetLinks() throws IOException{
+        String file = Files.readString(Path.of("test-file.md"));
+        assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(file));
     }
 }
