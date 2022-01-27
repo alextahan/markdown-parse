389a62cf62d2ca339207a90e021d18ecd771f688
alextahan <atahan@ucsd.edu>
2022-01-26 16:38:16 -0800

    Added more tests to MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 import org.junit.*;
 
-//javac -cp ".;lib/\*" MarkdownParseTest.java
+//javac -cp ".;lib/\*" MarkdownParseTest.java       //or use ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar"
 //java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest
 public class MarkdownParseTest {
     @Test
@@ -16,8 +16,26 @@ public class MarkdownParseTest {
     }
 
     @Test
-    public void testGetLinks() throws IOException{
+    public void testGetLinks1() throws IOException{
         String file = Files.readString(Path.of("test-file.md"));
         assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(file));
+    }
+
+    @Test
+    public void testGetLinks2() throws IOException{
+        String file = Files.readString(Path.of("test-file-2.md"));
+        assertEquals(List.of("something.html"), MarkdownParse.getLinks(file));
+    }
+
+    @Test
+    public void testGetLinks3() throws IOException{
+        String file = Files.readString(Path.of("test-file-3.md"));
+        assertEquals(List.of("something.html"), MarkdownParse.getLinks(file));
+    }
+
+    @Test
+    public void testGetLinks4() throws IOException{
+        String file = Files.readString(Path.of("test-file-4.md"));
+        assertEquals(List.of("something.html"), MarkdownParse.getLinks(file));
     }
 }
